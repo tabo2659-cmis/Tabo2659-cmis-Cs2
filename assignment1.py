77fa320f98be1ea16b3d413ed232866e228b78d1
cmis <cmis@cmis-hs02.(none)>
2016-01-29 10:56:23 +0700

    Age

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+myName = "Tabo"
+myAge = 14.09
+myHeightInMeters = 1.6256 
+oneSideOfSquare = 4 
+lengthOfTriangle = 5
+lengthOfTriangle = 8
+heightOfTriangle = 6
+myAgeInMonths = myAge * 12
+yearsLeftForMe = 76 - myAge
+myHeightInFeet = 5'4"
+myHeightInTandemWithIndiainMeters = myHeightInMeters
+

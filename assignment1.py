ed5f3f689e905ad053485a2fcfbe8b533a2293f8
cmis <cmis@cmis-hs02.(none)>
2016-02-01 13:35:39 +0700

    coomited

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,16 @@
 myName = "Tabo"
 myAge = 14.09
 myHeightInMeters = 1.6256 
-oneSideOfSquare = 4 
-lengthOfTriangle = 5
-lengthOfTriangle = 8
+sideOfSquare = 4 
+lengthOfTriangle = 8 
 heightOfTriangle = 6
 myAgeInMonths = myAge * 12
 yearsLeftForMe = 76 - myAge
-myHeightInFeet = 5'4"
-myHeightInTandemWithIndiainMeters = myHeightInMeters
+myHeightInFeet = myHeightInMeters * 3.2808
+myHeightInTandemWithIndiainMeters = 1.6509 - myHeightInMeters 
+areaOfSquare = 4 * 4
+halfVolumeOfCube = sideOfSquare ** 3/2.0
+areaOfTriangle = 1/2.0 * 8 * 6
+oneByNinthOfTheAreaOfTriangle = 1/9.0 * areaOfTriangle
+print "my name is" + str(myName) + "I am" + str(myAge) + "years old" + "I have" + str(yearsLeftForMe) + "to live" + "I am" +      
 

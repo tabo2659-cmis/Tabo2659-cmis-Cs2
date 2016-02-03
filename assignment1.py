5c959a51ab03601ef1374d09e12f85856ff5e948
cmis <cmis@cmis-hs02.(none)>
2016-02-03 08:20:19 +0700

    smiley

--- assignment1.py
+++ assignment1.py
@@ -1,16 +1,34 @@
 myName = "Tabo"
+#tells my name
 myAge = 14.09
-myHeightInMeters = 1.6256 
-sideOfSquare = 4 
-lengthOfTriangle = 8 
+#tells my aproximate age
+myHeightInMeters = 1.6256
+#tells my aproximate height in in meters   
+sideOfSquare = 4
+#A random number labelled as a the side of a square 
+lengthOfTriangle = 8
+#A random number labelled as the length of a triangle
 heightOfTriangle = 6
+#A random number labelled as the height of a triangle
 myAgeInMonths = myAge * 12
+#tells my age in months
 yearsLeftForMe = 76 - myAge
+#The number of years left for me to live on this beautiful planet
 myHeightInFeet = myHeightInMeters * 3.2808
+#tells my aproximate height in feet 
 myHeightInTandemWithIndiainMeters = 1.6509 - myHeightInMeters 
+#Tells the difference in my height and the average male indian teenager
 areaOfSquare = 4 * 4
+#tells the are of a square with the side of the number we wrote earlier
 halfVolumeOfCube = sideOfSquare ** 3/2.0
+#tells the volume of cube halfed as in divided by 2
 areaOfTriangle = 1/2.0 * 8 * 6
+#tells the area of a triangle
 oneByNinthOfTheAreaOfTriangle = 1/9.0 * areaOfTriangle
-print "my name is" + str(myName) + "I am" + str(myAge) + "years old" + "I have" + str(yearsLeftForMe) + "to live" + "I am" +      
-
+#tells one by ninth of the area of a triangle
+print "my name is" + str(myName) + "I am" + str(myAge) + "years old" + "I have" + str(yearsLeftForMe) + "to live" + "I am" + str(myHeightInMeters) + "tall in meters" + "in months I am" + str(myAgeInMonths) + "years old"      
+#prints out some variables using concantation (+) to make a sentence
+print "Hello my name is", (myName),"and the side of a square is", (sideOfSquare), "therefore it's area is",(areaOfSquare), "the length and height  of a triangle is",(lengthOfTriangle),"and",(heightOfTriangle), "so therefore it's area is",(areaOfTriangle)
+#prints out variable using "," to make a sentence
+print ";)" * 10000
+#prints out 10000 smiley faces,it's magnificient     

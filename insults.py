347f2f6ec83600d7b3c3c6e8a900e3e14aa5a7be
cmis <cmis@cmis-hs02.(none)>
2016-02-05 13:35:44 +0700

    hey

---
+++ insults.py
@@ -0,0 +1,26 @@
+def insults():
+	print"you momma so fat"
+	print"I missed"
+	print"three episodes when she crossed the tv"
+	print"you are ugly"
+	print"you are beautiful"
+	print"not sure how the last one is an insult"
+insults()
+
+def I_am(action):
+	print " I Am " + action + "ing"
+I_am("study") 
+I_am("eat")
+I_am("drink")
+
+def He_has(item1,item2):
+	print" He has a "+ item1
+	print" He also has " + item2 + " cool right"
+He_has("collecteble c3po","a dog")
+
+def describe(prop,factor):
+	
+
+
+
+

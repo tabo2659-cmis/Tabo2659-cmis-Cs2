b8d6963910be9784c4cb18a95a6eb070ed691414
cmis <cmis@cmis-hs02.(none)>
2016-02-09 08:40:58 +0700

    fun

---
+++ functions.py
@@ -0,0 +1,21 @@
+def add(a,b):
+	return a + b
+
+add(3,4)
+#A function that adds some numbers
+def sub(c,d):
+	return c - d
+
+sub(5,3)
+#A function that subtracts some numbers 
+def mul(x,y):
+	return x*y
+
+mul(4,4)
+##A function that multiplys some numbers
+def div(g,h):
+	return g/h
+
+div(2,3.0)
+##A function that divides some numbers  
+

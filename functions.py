e4cced8120814cd7ca9bf6356b8286fe61a8c904
cmis <cmis@cmis-hs02.(none)>
2016-02-10 10:55:45 +0700

    yay

--- functions.py
+++ functions.py
@@ -1,10 +1,11 @@
+import math
 def add(a,b):
 	return a + b
 
 add(3,4)
 #A function that adds some numbers
-def sub(c,d):
-	return c - d
+def sub(a,b):
+	return a - b
 
 sub(5,3)
 #A function that subtracts some numbers 
@@ -12,10 +13,19 @@ def mul(x,y):
 	return x*y
 
 mul(4,4)
-##A function that multiplys some numbers
+#A function that multiplys some numbers
 def div(g,h):
 	return g/h
 
 div(2,3.0)
-##A function that divides some numbers  
+#A function that divides some numbers 
+def SecsToHours(a,b,c):
+	return a/b/c
 
+SecsToHours(86400,60,60) 
+#A function that converts number of seconds into hours
+def Areaofacircle(r):
+	pi =(math.pi)
+	return pi * r**2
+	
+Areaofcircle

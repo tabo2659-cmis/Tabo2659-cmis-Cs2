eb4fcf0eeec850044237cf05f4fc6260088ba0c3
cmis <cmis@cmis-hs02.(none)>
2016-02-18 15:11:29 +0700

    im doing this

---
+++ simpleprogram.py
@@ -0,0 +1,16 @@
+
+x = raw_input("Type your name:")
+y = raw_input("Type ypur age:")	
+
+def hrslefttolive():
+	hrs = int(y) * 8760/2.0	
+	return hrs
+print str(hrslefttolive()) + " that is how many hours you have lived in years! "
+
+def moreinfo():
+	a = raw_input("Type a verb:")
+	b = raw_input("Do want a FERRAI!!??:")
+
+	 
+			
+

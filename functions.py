72270e8c98712d2255b9a6e9b37d49d80f9141a0
cmis <cmis@cmis-hs02.(none)>
2016-02-23 08:41:12 +0700

    whoo

--- functions.py
+++ functions.py
@@ -38,46 +38,47 @@ Volumeofsphere=(5)
 #A function that tells the volume of a sphere
 def averagevolume(a,b):
 	pi = (math.pi)
+	x = a/2
+	y = b/2
 	Vol1 = (((4/3.0)*pi)*a**3)
 	Vol2 = (((4/3.0)*pi)*b**3)
-	return Vol1 + Vol2/2
+	return (Vol1 + Vol2)/2
 
-averagevolume(5,10)
+averagevolume(10,20)
 #A function that tells the average voulume of two spheres
 def areaoftriangle(a,b,c):
 	p = a+b+c/2.0
 	return math.sqrt(p*(p-a)*(p-b)*(p-c))
 
-areaoftriangle(1,2,2.5)
+print areaoftriangle(1,2,2.5)
 #A function that tells the area of a triangle
 def right_align(a):
 	return (a.rjust(80))
-print right_align("Hello")
+right_align("Hello")
 #A function that aligns hello to the right side of python
 def center(a):
 	return (a.center(40))
 center("Hello")
 #A function that aligns hello to center of python
 def msgbox(a):
-	x =    """		  +---------+
-		  |  """+"Hello"+"""  |
-		  +---------+"""
-	print x
-	return"""		  +---------------+
-		  |  """+a+"""  |
-		  +---------------+"""
-	
-print msgbox("I eat cats!")
-# This function displays the word Hello and the sentence I eat cats! in a box 
-a=add(3,4)
-b=sub(5,3)
-c=mul(4,4)
-d=div(2,3.0)
-e=SecsToHours(86400,60,60)
-f=Areaofcircle=(5)
-g=Volumeofsphere=(5)
-h=averagevolume(5,10)
-i=areaoftriangle(1,2,2.5)
-j=right_align("Hello")
-k=center("Hello")
-l=msgbox("I eat cats!")
+    return "+" + (len(a) + 4) * "-" + "+\n" "|" + "  " + a + "  " + "|\n" "+" + (len(a) + 4) * "-" + "+"
+
+msgbox("Hello")
+msgbox("I eat cats")
+# This function displays the word Hello and the statement I eat cats in a box
+
+A = add(3,4)
+B = sub(5,3)
+C = mul(4,4)
+D = div(2,3.0)
+E = SecsToHours(86400,60,60)
+F = Areaofcircle=(5)
+G = Volumeofsphere=(5)
+H = averagevolume(5,10)
+I = areaoftriangle(1,2,2.5)
+J = right_align("Hello")
+K = center("Hello")
+L = msgbox("I eat cats!")
+# Stores all the functions in this file in a variable
+
+
--- simpleprogram.py
+++ simpleprogram.py
@@ -1,20 +1,22 @@
 
-x = raw_input("Type your name:")
-y = raw_input("Type ypur age:")	
+
 
 def hrslefttolive():
 	hrs = int(y) * 8760/2.0	
 	return hrs
 print str(hrslefttolive()) + " that is how many hours you have lived in years! "
-a = raw_input("Type a verb:")
-b = raw_input("Do want a FERRAI!!??:")
-c = raw_input("Write the verb in past tense:")
 
 def timeleft():
-        return 621,960 - int(hrslived())
-def main():
-        print " Before you get the results answer this question! " + raw_input("pi = ?") + " the answer is str(math.pi) if you got it right hooray!(3.14 works), if not you dumb " 
-	return " Hey! " + x + " who " + a + " how are you? I'm guessing you said yes to the Ferrari " + b + " you did didn't you?, if you didn't why not!? Anyways you have lived for exactly " + str(hrslived()) + " hours to this day on this beautiful planet and you only have " +  str(timeleft()) + "    
+	hrslefttolive()
+        return 621,960 - int(hrsleftotlive())
+
+def main(): 
+	x = raw_input("Type your name:")
+	y = raw_input("Type ypur age:")
+	a = raw_input("Type a verb in present tense:")
+	b = raw_input("Do want a FERRAI!!??:")
+	c = raw_input("Write the verb in past tense:")	
+	return " Hey! " + x + " who " + a + " how are you? I'm guessing you said yes to the Ferrari " + b + " you did didn't you?, if you didn't why not!? Anyways you have lived for exactly " + str(hrslefttolive()) + " hours to this day on this beautiful planet and you only have " +  str(timeleft()) + " left to live.....................GET A FERRARI!!!! thank you for answering these utterly meaning less questions! :) "    
        
 print main()
         

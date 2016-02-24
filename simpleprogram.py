d55c8e0d546ed3e65ca5d332e8a014dd8bb5276b
cmis <cmis@cmis-hs02.(none)>
2016-02-24 10:13:04 +0700

    done

--- simpleprogram.py
+++ simpleprogram.py
@@ -1,26 +1,26 @@
-
-
+def info():
+	y = raw_input("Type ypur age:")
+	return y
+info()
 
 def hrslefttolive():
-	hrs = int(y) * 8760/2.0	
+	hrs = int() * 8760/2.0	
 	return hrs
-print str(hrslefttolive()) + " that is how many hours you have lived in years! "
 
 def timeleft():
 	hrslefttolive()
         return 621,960 - int(hrsleftotlive())
+def concan(x,a,b):
+	return  " Hey! " + {} + " who " + {} + " how are you? I'm guessing you said yes to the Ferrari " + {} + " you did didn't you? ".format(x,a,b)
 
 def main(): 
 	x = raw_input("Type your name:")
-	y = raw_input("Type ypur age:")
 	a = raw_input("Type a verb in present tense:")
 	b = raw_input("Do want a FERRAI!!??:")
 	c = raw_input("Write the verb in past tense:")	
-	return " Hey! " + x + " who " + a + " how are you? I'm guessing you said yes to the Ferrari " + b + " you did didn't you?, if you didn't why not!? Anyways you have lived for exactly " + str(hrslefttolive()) + " hours to this day on this beautiful planet and you only have " +  str(timeleft()) + " left to live.....................GET A FERRARI!!!! thank you for answering these utterly meaning less questions! :) "    
-       
+	return str(concan(x,a,b)) + " if you didn't why not!? Anyways you have lived for exactly " + str(hrslefttolive()) + " hours to this day on this beautiful planet and you only have " +  str(timeleft()) + " left to live.....................GET A FERRARI!!!! thank you for answering these utterly meaning less questions! :) "         
 print main()
         
 
 	 
-			
-
+		

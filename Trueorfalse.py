67faa328fa2426d333a9541abd913c4578dc8c55
cmis <cmis@cmis-hs02.(none)>
2016-03-01 10:55:39 +0700

    Fav

---
+++ Trueorfalse.py
@@ -0,0 +1,21 @@
+print "Panic!AtTheDisco - Victorious!"
+
+def verse1():
+    print """Tonight we are Victorious! 
+Champange pouring over us!
+All my friends were glorious!
+Tonight we are Victorious!"""
+
+def verse2():
+    print """Double-bubble disco queen
+headed to the guillotine 
+skin as cool as steve-mecqueen 
+let me be your killer king"""
+
+def verse3():
+    print """It hurts until it stops
+we love until its not 
+I'm a killing spree in white 
+eyes like broken christmas lights"""
+
+def verse4():

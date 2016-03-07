eaf32cdde9417f7b334c16c244dc58eea0547c28
cmis <cmis@cmis-hs09.(none)>
2016-03-07 10:55:46 +0700

    Almost

--- simpleprogram.py
+++ simpleprogram.py
@@ -3,20 +3,24 @@ def hrslefttolive(yourage):
 	return hrs
 
 def timeleft(yourage):
-	z = hrslefttolive(yourage)
-        g = (621960 - float(z))
-	return g
+	Functioncall = hrslefttolive(yourage)
+        timeLeftforyou = (621960 - float(Functioncall))
+	return timeLeftforyou
 
-def concan(x,a,b):
-	return  " Hey! {} who {} how are you? I'm guessing you said yes to the Ferrari {} you did didn't you? ".format(x,a,b)
+
+def output(yourname,grammar,desires,hrslefttolive,timeleft):
+	 return " Hey! {} who {] how are you? I'm guessing you said yes to the Ferrari {] you did didn't you? " + " if you didn't why not!? Anyways you have lived for exactly {] hours to this day on this beautiful planet and you only have {} left to live.....................GET A FERRARI!!!! thank you for answering these utterly meaning less questions! :) ".format(yourname,grammar,desires,hrslefttolive,timeleft)
 
 def main(): 
 	yourage = raw_input("Type your age:")
 	yourname = raw_input("Type your name:")
 	grammar = raw_input("Type a verb in present continious tense:")
-	desires = raw_input("Do want a FERRAI!!??:") 	
-	return str(concan(yourname,grammar,desires)) + " if you didn't why not!? Anyways you have lived for exactly " + str(hrslefttolive(yourage)) + " hours to this day on this beautiful planet and you only have " +  str(timeleft(yourage)) + " left to live.....................GET A FERRARI!!!! thank you for answering these utterly meaning less questions! :) "         
-print main()
+	desires = raw_input("Do want a FERRAI!!??:")
+	timeleftforyou = str(timeleft(yourage))
+	hrsleftotlive = str(hrslefttolive(yourage))	
+	return output(yourname,grammar,desires,hrslefttolive,timeleft)
+         
+main()
         
 
 	 

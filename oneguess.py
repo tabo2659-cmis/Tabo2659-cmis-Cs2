0026d8cbd662cbba6bf070941fa44d3cb8ca495c
cmis <cmis@cmis-hs02.(none)>
2016-03-15 13:00:11 +0700

    DOne

--- oneguess.py
+++ oneguess.py
@@ -1,26 +1,30 @@
+import random
 def numberRange(number1,number2):
-    return random.randit(a,b)
+    return random.randint(number1,number2)
 
 def resultbyuser(result,usersGuess):
     if result > usersGuess:
-        sub = usersGuess - result
-        print "That's over by {}"
+        sub = abs(usersGuess - result)
+        return "That's over by {}".format(sub)
     else:
-        add = usersGuess + result
-        print "That's under by {}"
+        add = abs(usersGuess + result)
+        return "That's under by {}".format(add)
 
 def main():
     number1 = int(raw_input("What is the minimum number?"))
     number2 = int(raw_input("What is the maximum number?"))
-    print "Im thinking of a number from {} to {}".format(a,b)
+    print "Im thinking of a number from {} to {}".format(number1,number2)
     usersGuess = int(raw_input("What do you think it is?:"))
     result = int(numberRange(number1,number2))
+    wrong = resultbyuser(result,usersGuess)
     if result == usersGuess:
         print """The target was {}
 Your guess was {}
-That's correct! You must be a psychic!"""
+That's correct! You must be a psychic!""".format(result,usersGuess)
     else:
         print """The target was {}
 Your guess was {}
-{}"""
+{}""".format(result,usersGuess,wrong)
+
+main()
 

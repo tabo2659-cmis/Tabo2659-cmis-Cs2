456c3c8424ed9b569f03ec19438060650e7f8a00
cmis <cmis@cmis-hs20.(none)>
2016-03-17 14:59:45 +0700

    working

--- conditionals.py
+++ conditionals.py
@@ -1,2 +1,17 @@
-#My conditional program will run the user through a day of school life. It will give the users options in the beginning they will be given three options to choose which will determine when they arrive at school(they won't know the value of the options until chosen). After that the user will be able to decide her/his day with different options given to them(some of whose value they will and some which they won’t). The different options may be items that the user can eat for lunch and how they can answer the teacher in class, stuff like that. Depending on their grade, which will be inputted by the user, the teachers will be determined. All of these executions will be carried out using raw_input,relational operators and many functions probably. Python commands like import math and import random will come into play during math class. “”” “”” and     “ ”.format will automatically come into play during programming. 
+#My conditional program will run the user through a day of school life. It will give the users options in the beginning they will be given three options to choose which will determine when they arrive at school(they won't know the value of the options until chosen). After that the user will be able to decide her/his day with different options given to them(some of whose value they will and some which they wont). The different options may be items that the user can eat for lunch and how they can answer the teacher in class, how they talk to their friends(which conversation occurs an how the conversation carries on in the game will rely on the users input and previous choices), stuff like that. Depending on their grade, which will be inputted by the user, the teachers will be determined. Depending on the choices they make during the day they either will or will not go to Mr. Souza's office.
 
+def schooltime(arrival):
+    option1 = "You arrived late and missed first 5 mins of class"
+    option2 = "You arrived right on time but forgot to do homework"
+    option3 = "You arrived on time and are preapred for the day"
+    if arrival == A:
+        return option1
+    elif arrival == B:
+        return option2
+    else:
+        return option2
+
+def main():
+    arrival = raw_input("Pick one of these A,B,C your choice will determine what time you arrive at school")
+    arrived = schooltime(arrival)
+main()

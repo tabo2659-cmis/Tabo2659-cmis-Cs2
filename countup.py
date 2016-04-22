7d95675f3d9e4d842208430b278fb629275aeb5a
cmis <cmis@cmis-hs10.(none)>
2016-04-22 13:35:24 +0700

    interesting

--- countup.py
+++ countup.py
@@ -9,3 +9,29 @@ def main():
     countup(1)
 
 main()
+
+def countdown_from_to(start,stop):
+    if start == stop:
+        print "Blastoff!!"
+    elif start <= stop:
+        print "Invalid Pair"
+    else:
+        print start
+        countdown_from_to(start - 1,stop)
+
+def main():
+        countdown_from_to(16,32) 
+main()
+
+def adder(sum_):
+    print sum_
+    numb = raw_input("Next Number:")    
+    if numb == None:
+        print "The Sum Is {}".format(sum_) 
+    else:
+        adder(sum_)
+        
+def main():
+    sum_ = "Running Total:0"
+    adder(sum_)
+main()

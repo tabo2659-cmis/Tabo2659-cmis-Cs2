5facd99e7a088244bd52e3b7df248c08b7849ef8
cmis <cmis@cmis-hs10.(none)>
2016-04-28 14:47:51 +0700

    working

--- countup.py
+++ countup.py
@@ -1,19 +1,22 @@
-def adder(summation,biggest):
-    if float(numb) > float(numb):
+def great(summation,biggest,numb):
+    if float(biggest) > float(numb):
         biggest += float(numb)
+    else:
+        print "Hi"
 
-def adder(summation,biggest):
-    numb = raw_input("Next Number:")    
+def adder(summation,biggest,numb):
+    greatest = great(summation,biggest,numb)
     if numb == "":
-        print "The greatest is {}".format(str(biggest)) 
+        print "The greatest is {}".format(str(greatest)) 
     else:
         summation += float(numb)
         print "Running total: {}".format(str(summation))
-        adder(summation)        
+        adder(summation,biggest,numb)        
         
 def main(): 
+    numb = raw_input("Next Number:")    
     summation = 0
     biggest = 0
-    adder(summation,biggest)
+    adder(summation,biggest,numb)
 
 main()

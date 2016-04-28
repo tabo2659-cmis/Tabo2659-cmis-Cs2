26a2cb52fa6ea1f2f2920cd3bbfacc85b0ba3665
cmis <cmis@cmis-hs10.(none)>
2016-04-28 13:35:23 +0700

    working

--- countup.py
+++ countup.py
@@ -1,40 +1,19 @@
-def countup(n):
-    if n >= 10:
-        print "Blastoff!!"
-    else:
-        print n
-        countup(n+1)
+def adder(summation,biggest):
+    if float(numb) > float(numb):
+        biggest += float(numb)
 
-def main():
-    countup(1)
-
-main()
-
-def countdown_from_to(start,stop):
-    if start == stop:
-        print "Blastoff!!"
-    elif start <= stop:
-        print "Invalid Pair"
-    else:
-        print start
-        countdown_from_to(start - 1,stop)
-
-def main():
-        countdown_from_to(16,32) 
-main()
-
-def adder(summation):
-    numb = float(raw_input("Next Number:"))        
+def adder(summation,biggest):
+    numb = raw_input("Next Number:")    
     if numb == "":
-        print "The sum is {}".format(summation) 
-    elif numb == float:
-        print numb 
+        print "The greatest is {}".format(str(biggest)) 
     else:
         summation += float(numb)
-        print "Running total: {}".format(summation)
-        adder(summation)
+        print "Running total: {}".format(str(summation))
+        adder(summation)        
         
 def main(): 
-    adder(0)
+    summation = 0
+    biggest = 0
+    adder(summation,biggest)
 
 main()

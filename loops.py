6e1e7157df4f99f9af5abf279dba90949f496187
cmis <cmis@cmis-hs10.(none)>
2016-05-24 13:35:35 +0700

    s

---
+++ loops.py
@@ -0,0 +1,18 @@
+def countup(x):
+    while x >= 0:
+    	print x
+    	x -= 1
+countup(7)
+
+def countdown(y):
+    while y <= 10:
+    	print y
+    	y += 1
+countdown(6)
+
+def count(t):
+    while t > 0 or t < 0:
+        print t
+        t -= 1
+
+count(-11)

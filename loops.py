c5046b1a937a7b6ff4d114b543d4afe360f8b05f
cmis <cmis@cmis-hs10.(none)>
2016-05-27 10:55:52 +0700

    work

--- loops.py
+++ loops.py
@@ -1,44 +1,55 @@
-#def countup(x):
-#    while x >= 0:
-#    	print x
-#    	x -= 1
+def countup(x):
+    while x >= 0:
+    	print x
+    	x -= 1
 #countup(7)
 
-#def countdown(y):
-#    while y <= 10:
-#    	print y
-#    	y += 1
+def countdown(y):
+    while y <= 10:
+    	print y
+    	y += 1
 #countdown(6)
 
-#def count(t):
-#    while t > 0:
-#        print t
-#        t -= 1
-#    while t < 0:
-#        print t
-#        t += 1
+def count(t):
+    while t > 0:
+        print t
+        t -= 1
+    while t < 0:
+        print t
+        t += 1
 #count(-11)
 
-#def countfrom2(x,y):
-#    while x <= y:
-#        print x
-#        x += 1
-#    while x >= y:
-#        print x
-#        x -= 1
+def countfrom2(x,y):
+    while x <= y:
+        print x
+        x += 1
+    while x >= y:
+        print x
+        x -= 1
 #countfrom2(8,2)
 
 def addodds(n):
     sum_ = 0
     if n < 0:
         while n < 0:
-            if n % 2 != 0:
+            if n % 2 == 1:
                 sum_ += -n
-                n += 1
+            n += 1
     elif n > 0:
-        while n < 0:
-            if n % 2 != 0:
+        while n > 0:
+            if n % 2 == 1:
                 sum_ += n
-                n -= 1
-    print sum_    
-addodds(12)    
+            n -= 1
+    print sum_
+#addodds(-10)
+
+def grid(w):
+    row = "."*w
+    x = 0
+    while x < w:
+        row = "."*w
+        x += 1
+        print row
+
+grid(5)    
+          
